f4febece2b046b1bd1e5cbe24f3d87df51c6fa35
Elise Muellerleile <eliamue@live.com>
2022-01-09 17:35:02 -0600

    apieservice routes added

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,8 +1,8 @@
+const URL = "https://tbi-resources.herokuapp.com/api/v1/resources";
+
 export const fetchAllResources = async () => {
   try {
-    const res = await fetch(
-      "https://tbi-resources.herokuapp.com/api/v1/resources"
-    );
+    const res = await fetch(`${URL}`);
 
     const resources = await res.json();
     return resources.map((resource) => ({
@@ -16,4 +16,88 @@ export const fetchAllResources = async () => {
   } catch (error) {
     console.error(error.message);
   }
+};
+
+export const fetchOneResource = async (resourceId) => {
+  try {
+    const res = await fetch(
+      `${URL}/${resourceId}`
+    );
+    const {
+      id,
+      title,
+      category,
+      about,
+      link,
+      logo,
+    } = await res.json();
+
+    return {
+      id,
+      title,
+      category,
+      about,
+      link,
+      logo,
+    };
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+export const createResource = async (data) => {
+  try {
+    const res = await fetch(
+      `${URL}`,
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const {
+      id,
+      title,
+      category,
+      about,
+      link,
+      logo,
+    } = await res.json();
+
+    return {
+      id,
+      title,
+      category,
+      about,
+      link,
+      logo,
+    };
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+export const updateResource = async (id, updatedResource) => {
+  const res = await fetch(
+    `${URL}/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedResource),
+    }
+  );
+  return res.json();
+};
+
+export const deleteResource = (id) => {
+  return fetch(
+    `${URL}/${id}`,
+    { method: "DELETE" }
+  ).then((res) => res.json());
 };
