c19fc879ac7d1d9d866c74e6a563bd00d6850676
Elise Muellerleile <eliamue@live.com>
2022-02-22 03:27:52 -0600

    form styling

--- src/components/resources/AddForm.jsx
+++ src/components/resources/AddForm.jsx
@@ -46,118 +46,127 @@ const AddForm = () => {
     <>
       <Nav />
       <div className="form-container">
-        <h1>Add Resource</h1>
-        <div className="required">* Is Required</div>
+        <h1 className="add-header">Add Resource</h1>
+
         <form className="form" onSubmit={handleSubmit}>
-          
-          <label  className="form-section">
-            <div className="prompt">
-              <h4 className="required">*</h4>
-              <h4>Name of Resource: </h4>
-            </div>
-            <input
-              name="title"
-              onChange={handleTitle}
-              placeholder="Company Inc."
-              value={title}
-              required
-            />
-          </label>
 
-          <label className="form-section">
-            <div className="prompt">
-              <h4 className="required">*</h4>
-              <h4>Category: </h4>
-            </div>
-            <select
-              name="category"
-              onChange={handleCategory}
-              value={category}
-              required
-            >
-              <option value="" defaultValue disabled>
-                --
-              </option>
-              <option value="Accessibility" label="Accessibility"></option>
-              <option value="Advocacy" label="Advocacy"></option>
-              <option value="Education" label="Education"></option>
-              <option value="Legal" label="Legal"></option>
-              <option value="Products" label="Products"></option>
-              <option value="Services" label="Services"></option>
-              <option value="Other" label="Other"></option>
-            </select>
-          </label>
-
-          <label className="form-section">
-            <div className="prompt">
-              <h4 className="required">*</h4>
-              <h4>Description: </h4>
-            </div>
-            <textarea
-              name="about"
-              onChange={handleAbout}
-              value={about}
-              required
-              placeholder="Description of resource, such as services offered, symptoms addressed, location, etc."
-            />
-          </label>
-
-          <label className="form-section">
-            <div className="prompt">
-              <h4 className="required">*</h4>
-              <h4>Link to Resource: </h4>
-            </div>
-            <input
-              name="website"
-              onChange={handleWebsite}
-              value={website}
-              placeholder="http://www.tbi-resource.com"
-              required
-            />
-          </label>
-
-          <label className="form-section">
-            <div className="prompt">
-              <h4>Logo/Image</h4>
-            </div>
-            <input
-              name="logo"
-              onChange={handleLogo}
-              value={logo}
-              placeholder="http://www.tbi-resource.com"
-            />
-          </label>
-
-          <label className="form-section">
-            <div className="prompt">
-              <h4>Location: </h4>
-            </div>
-            <div className="options">
-              <States
-                value={located.value}
-                label={located.label}
-                onChange={handleLocated}
-                className="options"
+          <div className="info-container">
+            <label className="form-section">
+              <div className="prompt">
+                <h4 className="required">*</h4>
+                <h4>Name of Resource: </h4>
+              </div>
+              <input
+                name="title"
+                onChange={handleTitle}
+                placeholder="Company Inc."
+                value={title}
+                required
               />
-            </div>
-          </label>
+            </label>
 
-          <label className="form-section">
-            <div className="prompt">
-              <h4>Tags: </h4>
-            </div>
-            <div className="options">
-              <Tags
-                value={tags.tag}
-                label={tags.tag}
-                onChange={handleTags}
-                className="options"
+            <label className="form-section">
+              <div className="prompt">
+                <h4 className="required">*</h4>
+                <h4>Category: </h4>
+              </div>
+              <select
+                name="category"
+                className="categories"
+                onChange={handleCategory}
+                value={category}
+                required
+              >
+                <option value="" defaultValue disabled>
+                  Select...
+                </option>
+                <option value="Accessibility" label="Accessibility"></option>
+                <option value="Advocacy" label="Advocacy"></option>
+                <option value="Education" label="Education"></option>
+                <option value="Legal" label="Legal"></option>
+                <option value="Products" label="Products"></option>
+                <option value="Services" label="Services"></option>
+                <option value="Other" label="Other"></option>
+              </select>
+            </label>
+
+            <label className="form-section">
+              <div className="prompt">
+                <h4 className="required">*</h4>
+                <h4>Description: </h4>
+              </div>
+              <textarea
+                name="about"
+                id="about-field"
+                onChange={handleAbout}
+                value={about}
+                required
+                placeholder="Description of resource, such as services offered, symptoms addressed, location, etc."
               />
-            </div>
-          </label>
+            </label>
 
+            <label className="form-section">
+              <div className="prompt">
+                <h4 className="required">*</h4>
+                <h4>Link to Resource: </h4>
+              </div>
+              <input
+                name="website"
+                onChange={handleWebsite}
+                value={website}
+                placeholder="http://www.tbi-resource.com"
+                required
+              />
+            </label>
+
+            <label className="form-section">
+              <div className="prompt">
+                <h4>Logo/Image</h4>
+              </div>
+              <input
+                name="logo"
+                onChange={handleLogo}
+                value={logo}
+                placeholder="http://www.tbi-resource.com"
+              />
+            </label>
+          </div>
+      <div className="right-container">
+          <div className="options-container">
+            <label className="form-section">
+              <div className="prompt">
+                <h4>Location: </h4>
+              </div>
+              <div className="options">
+                <States
+                  value={located.value}
+                  label={located.label}
+                  onChange={handleLocated}
+                  className="options"
+                />
+              </div>
+            </label>
+
+            <label className="form-section">
+              <div className="prompt">
+                <h4>Tags: </h4>
+              </div>
+              <div className="options">
+                <Tags
+                  value={tags.tag}
+                  label={tags.tag}
+                  onChange={handleTags}
+                  className="options"
+                />
+              </div>
+            </label>
+          </div>
           <div>
-            <button type="submit" className="submit-btn">Submit</button>
+            <button type="submit" className="submit-btn">
+              Submit
+            </button>
+            <div className="required">* Is Required</div>
+            </div>
           </div>
         </form>
       </div>
